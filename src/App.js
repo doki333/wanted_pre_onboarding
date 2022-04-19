4f5f7df8e4ee9ee2eeaa51d3b8b2aa6d71e8d809
doki333 <duckky333@gmail.com>
2022-04-20 03:08:19 +0900

    remove command line

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <Toggle />
       <Tab />
-      {/* <Slider /> */}
-      {/* <Input /> */}
+      <Slider />
+      <Input />
       <Dropdown />
     </div>
   );
