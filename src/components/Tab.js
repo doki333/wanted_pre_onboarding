8d2430edfdcef092c2c68eb7907e38ea9541f52c
doki333 <duckky333@gmail.com>
2022-05-06 20:09:35 +0900

    edit:Tab.js

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,5 +1,38 @@
-import React from 'react'
+import { useRef } from 'react'
+import styles from './Tab.module.scss'
+
+const list = [
+  { position: 2, content: '감자' },
+  { position: 34, content: '고구마' },
+  { position: 66, content: '카레라이스' },
+]
 
 export default function Tab() {
-  return <div>Tab</div>
+  const moveRef = useRef()
+  const onClick = (e) => {
+    const { position } = e.target.dataset
+    moveRef.current.style.left = `${position}%`
+  }
+
+  return (
+    <section className={styles.tabWrapper}>
+      <h3>Tab</h3>
+      <ul className={styles.tabList}>
+        <span className={styles.movingBar} ref={moveRef} />
+        {list.map(({ position, content }) => {
+          return (
+            <button
+              type='button'
+              key={`food-${content}`}
+              data-position={position}
+              className={styles.tabItem}
+              onClick={onClick}
+            >
+              {content}
+            </button>
+          )
+        })}
+      </ul>
+    </section>
+  )
 }
