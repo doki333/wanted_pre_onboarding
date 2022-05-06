f54b19c8243482867fab19fcd81260f8818a45d1
doki333 <duckky333@gmail.com>
2022-05-07 00:52:11 +0900

    edit:Slider.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,6 @@
 import React from 'react'
+import styles from './Input.module.scss'
 
 export default function Input() {
-  return <div>Input</div>
+  return <section className={styles.inputWrapper}>Input</section>
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,64 @@
-import React from 'react'
+import { useCallback, useState } from 'react'
+import styles from './Slider.module.scss'
+
+const percentArr = [1, 25, 50, 75, 100]
 
 export default function Slider() {
-  return <div>Slider</div>
+  const [num, setNum] = useState(1)
+
+  const handleChange = useCallback((e) => {
+    setNum(e.currentTarget.value)
+  }, [])
+
+  const handleBtnClick = (e) => {
+    const { value } = e.currentTarget.dataset
+    setNum(value)
+  }
+
+  return (
+    <section className={styles.sliderWrapper}>
+      <h3>Slider</h3>
+      <div className={styles.textInputInner}>
+        <input type='text' value={num} readOnly id='textInput' />
+        <label htmlFor='textInput' className={styles.percentLabel}>
+          %
+        </label>
+      </div>
+      <span
+        style={{
+          background: `linear-gradient(
+      to right,
+      #17a2b8 0% ${num}%,
+      #fafafa ${num}% ${100 - num}%`,
+        }}
+        className={styles.rangeBlock}
+      >
+        <input
+          type='range'
+          step={1}
+          value={num}
+          onChange={handleChange}
+          min={1}
+          max={100}
+          className={styles.rangeInput}
+        />
+        <ul className={styles.numList}>
+          {percentArr.map((percent) => {
+            return (
+              <button
+                type='button'
+                key={`percent-${percent}`}
+                data-value={percent}
+                onClick={handleBtnClick}
+                className={styles.numBtn}
+              >
+                {percent}%
+                <span className={num > percent ? `${styles.isColored}` : `${styles.isNotColored}`} />
+              </button>
+            )
+          })}
+        </ul>
+      </span>
+    </section>
+  )
 }
